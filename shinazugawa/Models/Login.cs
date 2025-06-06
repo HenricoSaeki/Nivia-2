ea0f290602d715776ab94764297ad955c27e8c9f
Henrico&Hipo <henricorolim@gmail.com>
2025-06-06 10:19:37 -0300

    Primeiro Commit

---
+++ shinazugawa/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projetoJunho.Models
+{
+    public class Login
+    {
+        [Display(Name = "Usuário")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email Inválido")]
+        public string? Usuario { get; set; }
+
+        public string? Senha { get; set; }
+    }
+}
